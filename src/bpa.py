3f996615e6a53c275bdb5ff195f91c157b168d26
feightwywx <kururinmiracle@outlook.com>
2021-08-10 14:35:07 +0800

    报平安请求

--- src/bpa.py
+++ src/bpa.py
@@ -1,11 +1,13 @@
 from . import apiurl
 from .login import get_token
-import requests
+from .api.aes import aes_encrypt
 from requests.sessions import session
+from copy import copy
 import json
 
 
 def bpa(username, password):
+    print('学号 {} 同学开始报平安'.format(username))
     username = str(username)
     password = str(password)
     token = get_token(username, password)
@@ -28,7 +30,7 @@ def bpa(username, password):
 
     bpa_session = session()
 
-    # 界面请求，虽然没必要但是看起来可以合理一些 也不怎么拖速度（。
+    # 界面请求，虽然没必要
     view_resp = bpa_session.post(apiurl, data={
         'command': 'XGXT',
         'param': json.dumps({
@@ -50,3 +52,116 @@ def bpa(username, password):
         })
     }, headers=index_headers)
     print(stat_resp.status_code, stat_resp.text)
+    sm_code = stat_resp.json()['data']['sm']  # 报平安状态
+    if sm_code == '1':  # 未报平安
+        bpa_uri = 'http://bpa.haust.edu.cn/xgh5/stu/bpa/yq_info.html?' + \
+            aes_encrypt('xh={xh}&token={token}'.format(
+                xh=username, token=token
+            ))
+        bpa_headers = copy(index_headers)
+        bpa_headers['Referer'] = bpa_uri
+
+        enc_username = aes_encrypt(username)  # 学号你加密个锤子？
+        # 请求学生头像，虽然也没必要还耗时间
+        stupic_resp = bpa_session.post(apiurl, data={
+            'command': 'XGXT',
+            'param': json.dumps({
+                'cmd': 'stuPicView',
+                'xh': enc_username,
+                'token': token
+            })
+        }, headers=bpa_headers)
+        print(stupic_resp.status_code, '*** Student profile picture ***')
+
+        # 获取学生信息
+        getdata_resp = bpa_session.get(apiurl, params={
+            'command': 'XGXT',
+            'customercode': '',
+            'param': json.dumps({
+                'cmd': 'xsyqsbApplyView',
+                'xh': enc_username,
+                'token': token
+            })
+        }, headers=bpa_headers)
+        print(getdata_resp.status_code, getdata_resp.text)
+        studata = getdata_resp.json()['data']
+
+        # 报平安
+        commit_resp = bpa_session.post(apiurl, data={
+            'command': 'XGXT',
+            'param': json.dumps({
+                {
+                    'cmd': 'yqsbFormSave',
+                    'xh': studata['xh'],
+                    'sbsj': studata['sbsjS'],
+                    'nl': studata['nl'],
+                    'lxfs': studata['lxdh'],
+                    'jzdq': studata['jzdq'],
+                    'jzdq_xxdz': studata['jzdqXxdz'],
+                    'tw': '36.5',
+                    'sflx': '0',
+                    'jcbr': '0',
+                    'zyzz': '1,',
+                    'fbrq': '',
+                    'zyzzms': '',
+                    'bz': '',
+                    'bz1': '',
+                    'wcjtgj': '',
+                    'wcjtgjxq': '',
+                    'wcdq': '',
+                    'wcdqxxdz': '',
+                    'lkdate': '',
+                    'fhdate': '',
+                    'zszt': '',
+                    'ylzd1': '',
+                    'qrblxqdz': '',
+                    'qrbltjdz': '',
+                    'jcdq': '',
+                    'jcxxdz': '',
+                    'jcsj': '',
+                    'qzsj': '',
+                    'zlyy': '',
+                    'zysj': '',
+                    'token': token
+                }
+            })
+        }, headers=bpa_headers)
+        print(commit_resp.status_code, commit_resp.text)
+        commit_status = commit_resp.json()['result']
+        if commit_status == 0:  # 报平安成功
+            print('报平安成功')
+
+            # 请求报平安情况
+            bpa_list_uri = 'http://bpa.haust.edu.cn/xgh5/stu/bpa/yq_list.html?' + \
+                aes_encrypt('xh={xh}&token={token}'.format(
+                    xh=username, token=token
+                ))
+            bpa_list_headers = copy(index_headers)
+            bpa_list_headers['Referer'] = bpa_list_uri
+
+            list_resp = bpa_session.get(apiurl, params={
+                'command': 'XGXT',
+                'customercode': '',
+                'param': json.dumps({
+                    'cmd': 'xsyqsbApplyViewList',
+                    'xh': enc_username,
+                    'rqyf': '',
+                    'zsbts': '',
+                    'token': token
+                })
+            }, headers=bpa_list_headers)
+            print(list_resp.status_code, list_resp.text)
+            bpa_count = list_resp.json()['data']['zsbts']
+            bpa_mon_count = list_resp.json()['data']['bysbts']
+            print('总报平安次数 {} ，本月报平安 {} 次。'.format(
+                bpa_count, bpa_mon_count
+            ))
+            return 0
+        else:
+            pass
+    elif sm_code == '-1':
+        print('今日已报过平安')
+    else:
+        pass
+    print('报平安失败，请检查')
+    return -1
