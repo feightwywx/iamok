ecfa34ddbcb98c545ef7fec5f2c5ef51dd8bb74f
feightwywx <kururinmiracle@outlook.com>
2021-08-13 16:26:14 +0800

    可以指定一天内什么时候报平安 bpa_once()加了两个参数

--- schedule.py
+++ schedule.py
@@ -8,34 +8,54 @@ from src import logger
 from src.bpa import bpa
 from random import randint
 
-def bpa_once(username, password):
+def bpa_once(username, password, bpatime: tuple =(8, 0), interval=60):
     try:
         stat = bpa(username, password)
     except Exception as e:
         logger.critical(str(e))
         stat = -2
+    
+    now_time = time.time()
+    serial_now_time = time.localtime(now_time)
+    serial_next_time = time.struct_time((
+        serial_now_time[0],
+        serial_now_time[1],
+        serial_now_time[2],
+        bpatime[0],
+        bpatime[1],
+        serial_now_time[5],
+        serial_now_time[6],
+        serial_now_time[7],
+        serial_now_time[8]
+    ))
+    next_time = time.mktime(serial_next_time)
+    one_day_after = next_time + 86400 - now_time + randint(-3600, 3600)
+    half_hr_after = 1800 + randint(-900, 900)
+
     if stat == 0 or stat == -1:
         # 成功执行或已报，延时23~25小时
-        Timer(86400 + randint(-3600, 3600), bpa_once, (username, password)).start()
+        Timer(one_day_after, bpa_once, (username, password)).start()
         logger.info('{username} 已报平安。下次报平安时间：{time}'.format(
             username=username,
-            time=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time() + 86400))
+            time=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(now_time + one_day_after))
         ))
     else:
         # 失败，延时15~45分钟
-        Timer(1800 + randint(-900, 900), bpa_once, (username, password)).start()
-        logger.info('{username} 报平安失败，将在30分钟后重试。预计报平安时间：{time}'.format(
+        Timer(half_hr_after, bpa_once, (username, password)).start()
+        logger.info('{username} 报平安失败，将在一会之后重试。预计报平安时间：{time}'.format(
             username=username,
-            time=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time() + 1800))
+            time=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(now_time + half_hr_after))
         ))
+    # 阻塞60秒 防止高频访问报非法操作
+    time.sleep(interval)
 
 def gen_config():
     logger.warning('未找到可用配置文件。')
     config_json = json.dumps({
         'user': [
             {
-                'username': 123456,
-                'password': 123456
+                'username': '123456',
+                'password': '123456'
             }
         ]
     }, indent=4)
